f61735a5820996a46aec4a92c881257f6ae30f30
Mohit Jangra <mohitjangra12@gmail.com>
2024-12-18 02:06:49 +0530

    renamed file from day<number> to day<two_digit_number> i.e. day7 -> day07 and updated refenrances to input and test file in programs

---
+++ day01/day01.c
@@ -0,0 +1,178 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum { false, true } bool;
+
+void swap(int64_t *first, int64_t *second) {
+    int64_t temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+void bubble_sort(int64_t *arr, size_t len) {
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            if (arr[i] > arr[j]) {
+                swap(&arr[i], &arr[j]);
+            }
+        }
+    }
+}
+
+typedef struct {
+    int64_t *items;
+    size_t len;
+    size_t cap;
+} arraylist_t;
+
+void init_arraylist_t(arraylist_t *const al, const size_t cap);
+void append_arraylist_t(arraylist_t *const al, const size_t value);
+int64_t get_arraylist_t(arraylist_t *const al, const size_t index);
+void deinit_arraylist_t(arraylist_t *const al);
+void print_arraylist_t(arraylist_t *const al);
+
+void init_arraylist_t(arraylist_t *const al, const size_t cap) {
+    int64_t *arr = (int64_t *)malloc(cap * sizeof(int64_t));
+    if (arr == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    al->cap = cap;
+    al->len = 0;
+    al->items = arr;
+}
+
+void append_arraylist_t(arraylist_t *const al, const size_t value) {
+    if (al->len >= al->cap) {
+        int64_t *arr = (int64_t *)malloc(al->cap * 2 * sizeof(int64_t));
+        if (arr == NULL) {
+            exit(EXIT_FAILURE);
+        }
+        al->cap *= 2;
+        memcpy(arr, al->items, sizeof(int64_t) * al->len);
+        free(al->items);
+        al->items = arr;
+    }
+    al->items[al->len++] = value;
+}
+
+int64_t get_arraylist_t(arraylist_t *const al, const size_t index) {
+    if (index >= al->len) {
+        exit(EXIT_FAILURE);
+    }
+    return al->items[index];
+}
+
+void deinit_arraylist_t(arraylist_t *const al) {
+    free(al->items);
+    al->items = NULL;
+}
+
+void print_arraylist_t(arraylist_t *const al) {
+    for (size_t i = 0; i < al->len; i++) {
+        printf("%ld, ", al->items[i]);
+    }
+    printf("\n");
+}
+
+void test_array_t() {
+    arraylist_t arr;
+    init_arraylist_t(&arr, 2);
+    assert(arr.len == 0);
+    assert(arr.cap == 2);
+    printf("Array:\nlen: %lu\ncap: %lu\n", arr.len, arr.cap);
+    append_arraylist_t(&arr, 1);
+    append_arraylist_t(&arr, 2);
+    append_arraylist_t(&arr, 3);
+    append_arraylist_t(&arr, 4);
+    append_arraylist_t(&arr, 5);
+    print_arraylist_t(&arr);
+    printf("Array:\nlen: %lu\ncap: %lu\n", arr.len, arr.cap);
+    assert(get_arraylist_t(&arr, 0) == 1);
+    assert(get_arraylist_t(&arr, 1) == 2);
+    assert(get_arraylist_t(&arr, 2) == 3);
+    assert(get_arraylist_t(&arr, 3) == 4);
+    assert(get_arraylist_t(&arr, 4) == 5);
+    assert(arr.len == 5);
+    assert(arr.cap == 8);
+    deinit_arraylist_t(&arr);
+    assert(arr.items == NULL);
+}
+
+void test() {
+    char *testFile = "day01.test";
+    FILE *file = fopen(testFile, "r");
+    if (!file) {
+        fprintf(stderr, "filed to open file with error no: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    int64_t left_num, right_num;
+    arraylist_t left, right;
+
+    init_arraylist_t(&left, 6);
+    init_arraylist_t(&right, 6);
+
+    while (fscanf(file, "%ld %ld", &left_num, &right_num) > 0) {
+        append_arraylist_t(&left, left_num);
+        append_arraylist_t(&right, right_num);
+    };
+
+    print_arraylist_t(&left);
+    print_arraylist_t(&right);
+    printf("\n");
+    for (size_t i = 0; i < left.len; ++i) {
+        int64_t ln = get_arraylist_t(&left, i);
+        int64_t rn = get_arraylist_t(&right, i);
+        printf("%ld %ld\n", ln, rn);
+    }
+    printf("\n");
+
+    deinit_arraylist_t(&left);
+    deinit_arraylist_t(&right);
+    fclose(file);
+}
+
+int main() {
+    // test_array_t();
+    // test();
+    char *inputFile = "day01.input";
+    FILE *file = fopen(inputFile, "r");
+    int32_t dict[100000] = {0};
+
+    int64_t left_num, right_num;
+    arraylist_t left, right;
+
+    init_arraylist_t(&left, 1000);
+    init_arraylist_t(&right, 1000);
+
+    while (fscanf(file, "%ld %ld", &left_num, &right_num) > 0) {
+        append_arraylist_t(&left, left_num);
+        append_arraylist_t(&right, right_num);
+        dict[right_num] += 1;
+    };
+
+    // TODO: sort arraylist_t left and right
+    bubble_sort(left.items, left.len);
+    bubble_sort(right.items, right.len);
+
+    int64_t res = 0;
+    int64_t res2 = 0;
+    for (size_t i = 0; i < left.len; ++i) {
+        int64_t ln, rn;
+        ln = get_arraylist_t(&left, i);
+        rn = get_arraylist_t(&right, i);
+        res += labs(rn - ln);
+        res2 += labs(ln * dict[ln]);
+    }
+    printf("total distance: %ld\n", res);
+    printf("total similarity: %ld\n", res2);
+
+    deinit_arraylist_t(&left);
+    deinit_arraylist_t(&right);
+    fclose(file);
+    return 0;
+}
---
+++ day01/day01.py
@@ -0,0 +1,36 @@
+def run(fileName: str):
+    with open(fileName, "r") as inputFile:
+        lines = inputFile.readlines()
+        left = []
+        right = []
+        for line in lines:
+            nums = [word.strip() for word in line.split()]
+            left.append(int(nums[0]))
+            right.append(int(nums[1]))
+        left = sorted(left)
+        right = sorted(right)
+        total_distance = 0
+        num_count = {}
+        for i in range(len(left)):
+            total_distance += abs(right[i]-left[i])
+            if (num_count.get(right[i])):
+                num_count[right[i]] += 1
+            else:
+                num_count[right[i]] = 1
+
+        print("Part 1")
+        print(f"Total distance: {total_distance}")
+
+        total_similarity = 0
+        for num in left:
+            if (num_count.get(num)):
+                total_similarity += num * num_count.get(num)
+
+        print("Part 2")
+        print(f"Total similarity: {total_similarity}")
+
+
+if __name__ == "__main__":
+    # fileName = "day01.test"
+    fileName = "day01.input"
+    run(fileName)
